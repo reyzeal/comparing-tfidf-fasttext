7d4b2826485527491d2e33c901071e0ee3e4c813
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2022-08-13 15:03:48 +0700

    Checkpoint

--- methods/method/tfidf/module.py
+++ methods/method/tfidf/module.py
@@ -1,10 +1,12 @@
 import math
 
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 from database import retrieve
 from preprocessing import preprocessing
-
+from preprocessing.stopword import stopwords
 
 
 def bagOfWord(docs):
@@ -28,7 +30,8 @@ def termFrequency(bow):
     for judul in bow:
         temp = dict.fromkeys(judul.keys(), 0)
         for kata, total in judul.items():
-            temp[kata] = total / len(judul.keys())
+            if total > 0 :
+                temp[kata] = total / len([i for i in judul.keys() if judul[i] > 0])
         tf.append(temp)
     return tf
 
@@ -40,7 +43,7 @@ def inverseDocumentFrequency(docs):
             if tf > 0:
                 idf[word] += 1
     for word, value in idf.items():
-        idf[word] = math.log10(N/float(value)) if value > 0 else 1
+        idf[word] = math.log((1+N) / float(1+value)) + 1
     return idf
 
 def indexingTfidf():
@@ -59,7 +62,9 @@ def similarity(query):
 
 if __name__ == "__main__":
     data = retrieve()
-    judul = [i['preprocessed']['result'] for i in data]
+    # judul = [i['preprocessed']['result'] for i in data]
+    judul = [i.split(" ") for i in ["deteksi penyakit tuberculosis","deteksi dini penyakit jantung"]]
+    query = "deteksi penyakit jantung"
     bow = bagOfWord(judul)
     tf = termFrequency(bow)
     # print(len(bow), tf)
@@ -68,12 +73,23 @@ if __name__ == "__main__":
     tfidf = [calculateTfidf(i, idf) for i in tf]
     dftf = pd.DataFrame(tf)
     dftfidf = pd.DataFrame(tfidf)
-    dfidf = pd.DataFrame(idf, index=idf.keys())
+    dfidf = pd.DataFrame([idf])
+    print(dfidf)
     writer = pd.ExcelWriter("tfidf.xlsx", engine='xlsxwriter')
     dftf.to_excel(writer, sheet_name='TF')
     dfidf.to_excel(writer, sheet_name='IDF')
     dftfidf.to_excel(writer, sheet_name='TFIDF')
     writer.save()
     writer.close()
+
+    CV = CountVectorizer()
+    vectorizer = TfidfVectorizer(smooth_idf=True, use_idf=True)
+
+    word_count_vec = CV.fit_transform(["deteksi penyakit tuberculosis","deteksi dini penyakit jantung"])
+    tfidf = vectorizer.fit_transform(["deteksi penyakit tuberculosis","deteksi dini penyakit jantung"])
+    query_vec = vectorizer.transform([query])
+    results = cosine_similarity(tfidf, query_vec).reshape((-1,))
+    print(vectorizer.idf_, vectorizer.get_feature_names_out())
+    print(results)
     # print(tfidf)
     # print(bagOfWord(judul))
